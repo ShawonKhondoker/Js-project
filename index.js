0fdea2a5011b4195d0e2706ba355e1c5d75db1b7
ShawonKhondoker <shawonkhondoker0@gmail.com>
2023-02-21 11:29:08 -0700

    2n commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// var time = 12;
-// var boyes = 30;
-// var sal = 2023;
-// var address = 'kalna,"gournadi", barisal ';
-// var singal = false;
-// var phone = '01714956817';
-// let a = 2;
-// let b = 30;
-// const ip = 3.342553634;
-// const cars = ["Saab", "Volvo", "BMW"];
-// const date = new Date("2022-03-25");
+// start java-script
+var time = 12;
+var boyes = 30;
+var sal = 2023;
+var address = 'kalna,"gournadi", barisal ';
+var singal = false;
+var phone = '01714956817';
+let a = 2;
+let b = 30;
+const ip = 3.342553634;
+const cars = ["Saab", "Volvo", "BMW"];
+const date = new Date("2022-03-25");
 
 
 
-//  console.log(date, cars, time, address, singal, phone, a + b, ip);
+ console.log(date, cars, time, address, singal, phone, a + b, ip);
 
 
 
@@ -43,8 +44,9 @@ console.log(alurdam);
 alurdam /= 10;
 
 console.log(alurdam);
+
 var price=15;
-// price += 1;
+ price += 1;
 price++;
 console.log(price);
 
