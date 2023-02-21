628eb085c8467611fbbb1148cb70ef6817af274a
ShawonKhondoker <shawonkhondoker0@gmail.com>
2023-02-21 11:22:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// var time = 12;
+// var boyes = 30;
+// var sal = 2023;
+// var address = 'kalna,"gournadi", barisal ';
+// var singal = false;
+// var phone = '01714956817';
+// let a = 2;
+// let b = 30;
+// const ip = 3.342553634;
+// const cars = ["Saab", "Volvo", "BMW"];
+// const date = new Date("2022-03-25");
+
+
+
+//  console.log(date, cars, time, address, singal, phone, a + b, ip);
+
+
+
+// agar valu thakhay jok kore
+var alurdam = 20;
+alurdam = alurdam + 10;
+alurdam += 10;
+
+console.log(alurdam);
+
+//   agar valu thakhay bad kore
+ alurdam = 20;
+ alurdam = alurdam - 5;
+alurdam -= 10;
+
+console.log(alurdam);
+
+//   agar valu thakhay bad kore
+ alurdam = 20;
+ alurdam = alurdam * 5;
+alurdam *= 10;
+
+console.log(alurdam);
+
+//   agar valu thakhay bad kore
+ alurdam = 20;
+ alurdam = alurdam / 5;
+alurdam /= 10;
+
+console.log(alurdam);
+var price=15;
+// price += 1;
+price++;
+console.log(price);
+
+
